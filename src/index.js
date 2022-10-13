0ec05758c4d0ec5cb43d1be09b782819ba7d4834
Merit Dike <65596735+Meritdike@users.noreply.github.com>
2022-10-13 13:03:18 +0100

    ch: initial heading commit

--- src/index.js
+++ src/index.js
@@ -9,13 +9,13 @@ import './index.css';
 // Please delete these comments after using them
 // Here's a sample functional component to guide you in creating yours
 
-const Block = () => {
+const Block = ({heading, paragraph}) => {
     return (
         <div className='block w-full'>
-            <section className='py-10 w-full'>
-                <div className='container mx-auto py-10 bg-blue-300 rounded-lg px-8'>
-                    <h1 className='text-5xl font-bold text-center font-mono'>A random header</h1>
-                    <p className='text-xl mt-4'>Some random text</p>
+            <section className='w-full'>
+                <div className='mx-auto py-10 font-inter'>
+                    <h1 className='text-3xl font-bold text-left'>{heading}</h1>
+                    <p className='mt-2 font-normal text-left text-ash text-sizePara'>{paragraph}</p>
                 </div>
             </section>
         </div>
@@ -28,7 +28,9 @@ const Block = () => {
 const App = () => {
   return (
     <div className='app'>
-      <Block />
+      <Block heading={"Join Us!"}
+      paragraph={"To begin this journey, tell us what type of account you'd be opening."}
+      />
     </div>
   )
 }
